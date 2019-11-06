83824b11f544e2bccc99a6f80d5ecd85ee98b509
Dobbelsteen <satanderm@gmail.com>
2019-11-07 00:19:47 +0100

    Make it merge

--- engine/game_state/world_state.gd
+++ engine/game_state/world_state.gd
@@ -5,7 +5,7 @@ const CONNECT_ATTEMPTS: = 10 # Max amount of tries to get game state before taki
 var _connects_attempted: = 0
 
 # Kinda have to hardcode these for now.
-var START_SCENE: = "res://maps/overworld.tscn"
+var START_SCENE: = "res://maps/overworld.tmx"
 var START_SCENE_NAME: = "overworld"
 
 var original_state: = {}
